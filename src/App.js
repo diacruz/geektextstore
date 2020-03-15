b3303abe9db0b0921955749695d21c0f15c46628
Haczar <Haczar.Criollo@gmail.com>
2020-03-14 20:22:49 -0400

    - added a placeholder page in wishlist link

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from './Components/NavigationBar/Navbar'
 import Books from './Components/BookSearch/Books'
 import Cart from './Components/ShoppingCart/Cart'
 import Home from './Components/Home'
+import Wishlist from './Components/WishListManagement/Wishlist'
 
 class App extends Component {
     render() {
@@ -17,6 +18,7 @@ class App extends Component {
                         <Route path="/" component={Home} exact />
                         <Route path="/home" component={Home} exact />
                         <Route path="/search" component={Books} exact />
+						<Route path="/wishlist" component={Wishlist} exact />
                         <Route path="/cart" component={Cart} exact />
                         <Route path="/book-details/:id" component={BookDetails} />
                     </Switch>
