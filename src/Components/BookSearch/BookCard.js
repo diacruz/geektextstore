e0424c2684a966a10746bf8059e8fdb0a4792156
Dianet <dianetcruzz@hotmail.com>
2020-04-12 00:17:34 -0400

    improved book list presentation

--- src/Components/BookSearch/BookCard.js
+++ src/Components/BookSearch/BookCard.js
@@ -3,13 +3,23 @@ import { makeStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 import Books from './Books'
 import { bookReducer } from '../ShoppingCart/bookReducer'
+import { CardContent, CardActions, Card } from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
     btn: {
         margin: theme.spacing(1),
     },
+    cardContainer: {
+        margin: theme.spacing(2),
+        maxWidth: 270,
+        display: 'flex',
+        flexFlow: 'column',
+        justifyContent: 'space-between',
+    },
+    cardActions: {
+        bottom: 0,
+    },
 }))
-
 
 const BookCard = (props) => {
     const {
@@ -21,22 +31,24 @@ const BookCard = (props) => {
         published,
         bookId,
         price,
-        dummyrating
+        dummyrating,
     } = props
 
     const classes = useStyles()
 
-    
-    
     return (
-        <div className="card-container">
-            <img src={image} alt="" />
-            <div className="desc">
-                <h2>{title}</h2>
-                <h3>{author}</h3>
-                <p>{published}</p>
-                <p>Price: {price}</p>
-                <p>Rating: {dummyrating}</p>
+        <Card className={classes.cardContainer}>
+            <CardContent>
+                <img src={image} alt="" />
+                <div className="desc">
+                    <h2>{title}</h2>
+                    <h3>{author}</h3>
+                    <p>{published}</p>
+                    <p>Price: {price}</p>
+                    <p>Rating: {dummyrating}</p>
+                </div>
+            </CardContent>
+            <CardActions className={classes.cardActions}>
                 <Button
                     size="small"
                     className={classes.btn}
@@ -49,14 +61,18 @@ const BookCard = (props) => {
                 <Button
                     size="small"
                     className={classes.btn}
-                    onClick={addToCart([props.title,props.author,props.price])}
+                    onClick={addToCart([
+                        props.title,
+                        props.author,
+                        props.price,
+                    ])}
                     variant="outlined"
                     color="primary"
                 >
                     Add to Cart
                 </Button>
-            </div>
-        </div>
+            </CardActions>
+        </Card>
     )
 }
 
--- src/Components/BookSearch/BookList.js
+++ src/Components/BookSearch/BookList.js
@@ -1,211 +1,189 @@
 import React, { useState, useReducer, useEffect, useContext } from 'react'
 import BookCard from './BookCard'
 import { useHistory } from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles'
 import bookId from './BookCard'
 import bookReducer from '../ShoppingCart/bookReducer'
 
+const useStyles = makeStyles((theme) => ({
+    list: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+}))
 
 const BookList = (props) => {
     let history = useHistory()
+    const classes = useStyles()
 
-
-    const handleOpenDetails = id => () => history.push(`/book-details/${id}`)
+    const handleOpenDetails = (id) => () => history.push(`/book-details/${id}`)
     console.log(props)
-  
-     const handleAddToCart = (book) => () => {
+
+    const handleAddToCart = (book) => () => {
         localStorage.setItem('books', JSON.stringify(book))
         console.log(book)
-    } 
-
-
-    var bookspp = parseInt(props.books.bookspp);
-    var rating = parseInt(props.books.rating);
-
-    if (isNaN(bookspp))
-    {
-        bookspp = 10;
-    }
-    if (isNaN(rating))
-    {
-        rating = 0;
     }
 
-    console.log(rating);
-    var sortedBooks = props.books.books;
+    var bookspp = parseInt(props.books.bookspp)
+    var rating = parseInt(props.books.rating)
+
+    if (isNaN(bookspp)) {
+        bookspp = 10
+    }
+    if (isNaN(rating)) {
+        rating = 0
+    }
+
+    console.log(rating)
+    var sortedBooks = props.books.books
 
     var sorttitleasc = function (a, b) {
-        a = a.title.toLowerCase();
-        b = b.title.toLowerCase();
-        if (a < b) return 1;
-        if (a > b) return -1;
-        return 0;
+        a = a.title.toLowerCase()
+        b = b.title.toLowerCase()
+        if (a < b) return 1
+        if (a > b) return -1
+        return 0
     }
 
     var sorttitledes = function (a, b) {
-        a = a.title.toLowerCase();
-        b = b.title.toLowerCase();
-        if (a < b) return -1;
-        if (a > b) return 1;
-        return 0;
+        a = a.title.toLowerCase()
+        b = b.title.toLowerCase()
+        if (a < b) return -1
+        if (a > b) return 1
+        return 0
     }
 
     var sortauthorasc = function (a, b) {
-        a = a.authors[0].toLowerCase();
-        b = b.authors[0].toLowerCase();
-        if (a < b) return 1;
-        if (a > b) return -1;
-        return 0;
+        a = a.authors[0].toLowerCase()
+        b = b.authors[0].toLowerCase()
+        if (a < b) return 1
+        if (a > b) return -1
+        return 0
     }
 
     var sortauthordes = function (a, b) {
-        a = a.authors[0].toLowerCase();
-        b = b.authors[0].toLowerCase();
-        if (a < b) return -1;
-        if (a > b) return 1;
-        return 0;
+        a = a.authors[0].toLowerCase()
+        b = b.authors[0].toLowerCase()
+        if (a < b) return -1
+        if (a > b) return 1
+        return 0
     }
 
     var sortdateasc = function (a, b) {
-        a = (a.publishedDate + "").substring(0, 10);
-        b = (b.publishedDate + "").substring(0, 10);
-        if (a == "undefined")
-        {
+        a = (a.publishedDate + '').substring(0, 10)
+        b = (b.publishedDate + '').substring(0, 10)
+        if (a == 'undefined') {
             a = ''
         }
-        if (a < b) return 1;
-        if (a > b) return -1;
-        return 0;
+        if (a < b) return 1
+        if (a > b) return -1
+        return 0
     }
 
     var sortdatedes = function (a, b) {
-        a = (a.publishedDate + "").substring(0, 10);
-        b = (b.publishedDate + "").substring(0, 10);
-        if (a < b) return -1;
-        if (a > b) return 1;
-        return 0;
+        a = (a.publishedDate + '').substring(0, 10)
+        b = (b.publishedDate + '').substring(0, 10)
+        if (a < b) return -1
+        if (a > b) return 1
+        return 0
     }
 
     var sortpricedes = function (a, b) {
-        a= a.price;
-        b= b.price;
-        if (a < b) return 1;
-        if (a > b) return -1;
-        return 0;
+        a = a.price
+        b = b.price
+        if (a < b) return 1
+        if (a > b) return -1
+        return 0
     }
 
-    var sortpriceasc = function (a, b) {  
-        a= a.price;
-        b= b.price;      
-        if (a < b) return -1;
-        if (a > b) return 1;
-        return 0;
+    var sortpriceasc = function (a, b) {
+        a = a.price
+        b = b.price
+        if (a < b) return -1
+        if (a > b) return 1
+        return 0
     }
 
     var sortratingdes = function (a, b) {
-        a= a.dummyrating;
-        b= b.dummyrating;
-        if (a < b) return 1;
-        if (a > b) return -1;
-        return 0;
+        a = a.dummyrating
+        b = b.dummyrating
+        if (a < b) return 1
+        if (a > b) return -1
+        return 0
     }
 
-    var sortratingasc = function (a, b) {  
-        a= a.dummyrating;
-        b= b.dummyrating;      
-        if (a < b) return -1;
-        if (a > b) return 1;
-        return 0;
+    var sortratingasc = function (a, b) {
+        a = a.dummyrating
+        b = b.dummyrating
+        if (a < b) return -1
+        if (a > b) return 1
+        return 0
     }
 
-    if (props.books.sortfields == "title")
-    {
-        if (props.books.order == "des") {
-            sortedBooks = sortedBooks.sort(sorttitledes);
+    if (props.books.sortfields == 'title') {
+        if (props.books.order == 'des') {
+            sortedBooks = sortedBooks.sort(sorttitledes)
         } else {
             sortedBooks = sortedBooks.sort(sorttitleasc)
         }
-    }
-    else if (props.books.sortfields == "author")
-    {
-        if (props.books.order == "des") {
-            sortedBooks = sortedBooks.sort(sortauthordes);
+    } else if (props.books.sortfields == 'author') {
+        if (props.books.order == 'des') {
+            sortedBooks = sortedBooks.sort(sortauthordes)
         } else {
             sortedBooks = sortedBooks.sort(sortauthorasc)
         }
-    }
-    else if (props.books.sortfields == "published-date")
-    {
-        if (props.books.order == "des") {
-            sortedBooks = sortedBooks.sort(sortdatedes);
+    } else if (props.books.sortfields == 'published-date') {
+        if (props.books.order == 'des') {
+            sortedBooks = sortedBooks.sort(sortdatedes)
         } else {
             sortedBooks = sortedBooks.sort(sortdateasc)
         }
-    }
-    else if (props.books.sortfields == "price")
-    {
-        if (props.books.order == "des") {
-            sortedBooks = sortedBooks.sort(sortpricedes);
+    } else if (props.books.sortfields == 'price') {
+        if (props.books.order == 'des') {
+            sortedBooks = sortedBooks.sort(sortpricedes)
         } else {
             sortedBooks = sortedBooks.sort(sortpriceasc)
         }
-    }
-    else if (props.books.sortfields == "rating")
-    {
-        if (props.books.order == "des") {
-            sortedBooks = sortedBooks.sort(sortratingdes);
+    } else if (props.books.sortfields == 'rating') {
+        if (props.books.order == 'des') {
+            sortedBooks = sortedBooks.sort(sortratingdes)
         } else {
             sortedBooks = sortedBooks.sort(sortratingasc)
         }
     }
 
-    var counter = 0;
+    var counter = 0
 
     return (
-        <div className="list">
-            {
-                sortedBooks.map((book, i) => {
-                    if (book && counter < bookspp) 
-                    {
-                        if (book.dummyrating < rating)
-                        {
-                            return undefined;
-                        }
+        <div className={classes.list}>
+            {sortedBooks.map((book, i) => {
+                if (book && counter < bookspp) {
+                    if (book.dummyrating < rating) {
+                        return undefined
+                    }
 
-                        var date = book.publishedDate + "";
-                        date = date.substring(0, 10);
+                    var date = book.publishedDate + ''
+                    date = date.substring(0, 10)
 
-                        var rat = "⭐";
+                    var rat = '⭐'
 
-                        for (let index = 0; index < Math.floor(parseInt(book.dummyrating)) - 1; index++) {
-                            rat = rat + "⭐";                            
-                        }
+                    for (
+                        let index = 0;
+                        index < Math.floor(parseInt(book.dummyrating)) - 1;
+                        index++
+                    ) {
+                        rat = rat + '⭐'
+                    }
 
-
-
-                        if (props.books.filterfields != "" && props.books.filterfields != "null")
-                        {
-                            if (book.categories[0] == props.books.filterfields || book.categories[1] == props.books.filterfields)
-                            {
-                                counter++;
-                                return (
-                                    <BookCard
-                                        openDetails={handleOpenDetails}
-                                        addToCart={handleAddToCart}
-                                        key={i}
-                                        image={book.thumbnailUrl}
-                                        title={book.title}
-                                        author={book.authors[0]}
-                                        published={book.publishedDate}
-                                        bookId={book._id}
-                                        price={"$" + book.price}
-                                        rating={rat}
-                                    />
-                                )
-                            }
-                        } 
-                        else
-                        {
-                            counter++;
+                    if (
+                        props.books.filterfields != '' &&
+                        props.books.filterfields != 'null'
+                    ) {
+                        if (
+                            book.categories[0] == props.books.filterfields ||
+                            book.categories[1] == props.books.filterfields
+                        ) {
+                            counter++
                             return (
                                 <BookCard
                                     openDetails={handleOpenDetails}
@@ -216,13 +194,30 @@ const BookList = (props) => {
                                     author={book.authors[0]}
                                     published={book.publishedDate}
                                     bookId={book._id}
-                                    price={"$" + book.price}
+                                    price={'$' + book.price}
                                     rating={rat}
                                 />
                             )
-                        }                   
+                        }
+                    } else {
+                        counter++
+                        return (
+                            <BookCard
+                                openDetails={handleOpenDetails}
+                                addToCart={handleAddToCart}
+                                key={i}
+                                image={book.thumbnailUrl}
+                                title={book.title}
+                                author={book.authors[0]}
+                                published={book.publishedDate}
+                                bookId={book._id}
+                                price={'$' + book.price}
+                                rating={rat}
+                            />
+                        )
                     }
-                    return undefined
+                }
+                return undefined
             })}
         </div>
     )
